1351ed9784d0c126d3193f668762505e5ccc83b7
Amirenanta <129699123+Amirenanta@users.noreply.github.com>
2023-04-23 02:32:00 +0700

    Done

--- 1-conan.cpp
+++ 1-conan.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    int a;
-    std::cout << "Masukan Nama = " << std::endl;
-    cin >> a;
-
-    if (a == 3)
-    {
-        cout << "hola" << endl;
+int main()
+{
+   string nama;
+   std::cout << "Masukan Nama Suspect=" << std::endl;
+    std::cin >> nama;
+    int umur;
+   std::cout << "Masukan Umur Suspect =" << std::endl;
+    std::cin >> umur;
+    string tempatTinggal;
+   std::cout << "Masukan Tempat Tinggal Suspect =" << std::endl;
+    std::cin >> tempatTinggal;
+    int tabungan;
+   std::cout << "Masukan Jumlah Uang Tabungan Suspect (dalam US dolar) = " << std::endl;
+    std::cin >> tabungan;
+    
+if (nama == "Underboss" || "Don" || "Capo"){
+      if ((nama == "Don") && (umur >= 40) && (tempatTinggal == "New York" || "Havana" || "Nevada") && (tabungan > 10000000)) {
+        std::cout << nama << " Kemungkinan adalah seorang anggota mafia dengan [pangkat]." << std::endl;
     }
-
-
-    cout << a << endll;
-
-
+    else if ((nama == "Underboss") && (25 <= umur <= 40) && (tempatTinggal == "New Jersey" || "Manhattan" || "Nevada") && (1000000 < tabungan < 2000000)) {
+        std::cout << nama << " Kemungkinan adalah seorang anggota mafia dengan [pangkat]." << std::endl;
+    }
+    else if ((nama == "Capo") && (18 <= umur <= 24) && (tempatTinggal == "California" || "Detroit" || "Boston") && (tabungan > 10000000)) {
+        std::cout << nama << " Kemungkinan adalah seorang anggota mafia dengan [pangkat]." << std::endl;
+    }
+    
+     else std::cout << nama << " Tidak Mencurigakan." << std::endl;
+ }
     cin.get();
     return 0;
 }
