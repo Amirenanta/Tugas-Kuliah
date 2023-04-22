c71d1ddf3f0b5f306cd418783eba44fcab0b6443
Amirenanta <129699123+Amirenanta@users.noreply.github.com>
2023-04-23 01:36:14 +0700

    okok

---
+++ 1-conan.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    std::cout << "Masukan Nama = " << std::endl;
+    cin >> a;
+
+    if (a == 3)
+    {
+        cout << "hola" << endl;
+    }
+
+
+    cout << a << endll;
+
+
+    cin.get();
+    return 0;
+}
