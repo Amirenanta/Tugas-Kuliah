2e89c2624b79c6ed15d8c34f912239b12b65a29a
Amirenanta <129699123+Amirenanta@users.noreply.github.com>
2023-04-23 12:44:49 +0700

    some finishing touch

--- Tugas-Kondisi-Percabangan/2-interviewTest.cpp
+++ Tugas-Kondisi-Percabangan/2-interviewTest.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main(){
     int nilaiCoding;
-   std::cout << "Masukan Nilai Coding =" << std::endl;
+   std::cout << "Masukan Nilai Coding (0 - 100) =" << std::endl;
     std::cin >> nilaiCoding;
     char nilaiInterview;
-   std::cout << "Masukan Nilai Interview =" << std::endl;
+   std::cout << "Masukan Nilai Interview (A/B/C/D/F) =" << std::endl;
     std::cin >> nilaiInterview;
 
-if (nilaiCoding > 60 && (nilaiInterview == 'A' || nilaiInterview == 'B')) {
+if (nilaiCoding > 60 && nilaiCoding <= 100 && (nilaiInterview == 'A' || nilaiInterview == 'B')) {
         std::cout << "Selamat Kamu Berhasil Menjadi Calon Programmer!" << std::endl;
     } else {
         std::cout << "Maaf Kamu Belum Berhasil Menjadi Calon Programmer." << std::endl;
