96707b523799b2f6883d43d8ece569241f61206e
Amirenanta <129699123+Amirenanta@users.noreply.github.com>
2023-05-03 22:01:34 +0700

    Last update

--- Tugas-Kondisi-Percabangan/2-interviewTest.cpp
+++ Tugas-Kondisi-Percabangan/2-interviewTest.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+void coding(int nilaiCoding) {
+    if (nilaiCoding >= 80) {
+        std::cout << "LOLOS" << std::endl;
+    } else if (nilaiCoding >= 60) {
+        std::cout << "DIPERTIMBANGKAN" << std::endl;
+    } else {
+        std::cout << "GAGAL" << std::endl;
+    }
+}
+
+void interview(char nilaiInterview) {
+    if (nilaiInterview == 'A' || nilaiInterview == 'B') {
+        std::cout << "LOLOS" << std::endl;
+    } else {
+        std::cout << "GAGAL" << std::endl;
+    }
+}
+
 int main(){
-    int nilaiCoding;
    std::cout << "Masukan Nilai Coding (0 - 100) =" << std::endl;
+   int nilaiCoding;
     std::cin >> nilaiCoding;
-    char nilaiInterview;
    std::cout << "Masukan Nilai Interview (A/B/C/D/F) =" << std::endl;
+   char nilaiInterview;
     std::cin >> nilaiInterview;
 
 if (nilaiCoding > 60 && nilaiCoding <= 100 && (nilaiInterview == 'A' || nilaiInterview == 'B')) {
